7880285c38dc265291e77bf9e336b0c2e0e2e404
creeperyang <yangcreeper@hotmail.com>
2019-06-05 16:35:25 +0800

    feat: add main function

---
+++ bin.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const chalk = require('chalk');
+const { getPortUsageInfo, getProcessName, killProcess } = require('./index');
+
+const MSGS = isCNLanguage() ? {
+        warnNoPort: '请指定端口',
+        warnNoMatch: '指定端口无进程监听',
+        missArgs: '缺少参数：',
+        result: '端口匹配结果如下：',
+        killProcess: '结束以下进程：',
+        itemKill: '是否自动杀死对应进程',
+        itemFuzzy: '是否模糊匹配端口',
+    } : {
+        warnNoPort: 'specify the port please',
+        warnNoMatch: 'No process bound to the specified port(s)',
+        missArgs: 'Miss arguments:',
+        result: 'The search result is:',
+        killProcess: 'Now will kill the corresponding process(es):',
+        itemKill: 'whether to kill process',
+        itemFuzzy: 'match the port fuzzily',
+    };
+
+const LOG_PREFIX = '\r\n\t';
+const LOG_PREFIX2 = '\t';
+
+function isCNLanguage() {
+    const env = process.env;
+    const lang = env.LANG || env.LANGUAGE || env.LC_ALL || env.LC_MESSAGES;
+    if (lang) {
+        return 'zh_CN.UTF-8' === lang;
+    }
+    if (typeof Intl === 'object' && Intl.Collator) {
+        return Intl.Collator.supportedLocalesOf(['zh-CN']).length === 1;
+    }
+    return false;
+}
+
+program
+    .version('0.0.1')
+    .option('-k, --kill', 'whether to kill process')
+    .option('-f, --fuzzy', 'match the port fuzzily');
+
+program.parse(process.argv);
+
+const ports = program.args;
+
+if (!ports.length) {
+    console.log(
+        LOG_PREFIX +
+            chalk.grey(MSGS.missArgs) +
+            ' ' +
+            chalk.bold.bgRed(MSGS.warnNoPort)
+    );
+    return;
+}
+
+checkPort({ ports: ports, autoKill: program.kill, exact: !program.fuzzy });
+
+function checkPort({ ports, local = true, exact = true, autoKill = false }) {
+    return Promise.all(ports.map(port => getPortUsageInfo(port, local, exact)))
+        .then(lists => {
+            return lists.reduce((acc, cur) => acc.concat(cur), []);
+        })
+        .then(list => {
+            if (!list || !list.length) {
+                throw 'no-match';
+            }
+            return Promise.all(
+                list.map(v => {
+                    return v.pid
+                        ? getProcessName(v.pid).then(({ name }) => {
+                              v.name = name || '';
+                              return v;
+                          })
+                        : v;
+                })
+            );
+        })
+        .then(list => {
+            console.log(LOG_PREFIX + chalk.bgGreen.bold(MSGS.result));
+            // PID: 10, State: 11+3, Address: 15+5+5, Name: 20
+            const header =
+                LOG_PREFIX2 +
+                'PID'.padEnd(10) +
+                'State'.padEnd(14) +
+                'Address'.padEnd(25) +
+                'Name'.padEnd(16);
+            console.log(chalk.grey.bold.underline(header));
+
+            const pids = {};
+            list.forEach(v => {
+                const str =
+                    LOG_PREFIX2 +
+                    v.pid.padEnd(10) +
+                    v.state.padEnd(14) +
+                    (local ? v.localAddress : v.remoteAddress).padEnd(25) +
+                    v.name;
+                pids[v.pid] = v.name;
+                console.log(chalk.white(str));
+            });
+
+            return autoKill ? pids : {};
+        })
+        .then(list => {
+            const pids = Object.keys(list);
+            if (!pids.length) return;
+
+            console.log(LOG_PREFIX + chalk.bgGreen.bold(MSGS.killProcess));
+            pids.forEach(pid => {
+                killProcess(pid).then(() => {
+                    console.log(
+                        LOG_PREFIX2 +
+                            chalk.bold.white(pid.padEnd(10)) +
+                            '\t' +
+                            chalk.bold.green('√')
+                    );
+                });
+            });
+        })
+        .catch(reason => {
+            if (reason.toString() === 'no-match') {
+                console.log(LOG_PREFIX + chalk.bold.green(MSGS.warnNoMatch));
+            } else {
+                console.log(formatErrorMsg(reason));
+            }
+        });
+}
+
+function formatErrorMsg(err) {
+    if (err instanceof Error) {
+        return (
+            LOG_PREFIX2 +
+            chalk.bold.bgRed(err.message) +
+            LOG_PREFIX +
+            chalk.red(err.stack)
+        );
+    }
+    return LOG_PREFIX2 + chalk.bold.red(err.toString());
+}
---
+++ index.js
@@ -0,0 +1,84 @@
+const { exec } = require('child_process');
+
+function getPort(address) {
+    if (!address) return '';
+    const port = /:(\d+)$/.exec(address);
+    return port ? port[1] : '';
+}
+
+function filterWithPort(info, port, local, exact) {
+    if (!info || !info.localAddress) return false;
+    const portToCompare = local ? info.localPort : info.remotePort;
+    return exact ? portToCompare === port
+        : portToCompare.includes(port);
+}
+
+/**
+ * Parse cli message to prot info.
+ * @param {string} line the input string to parse
+ * @returns {Object} the parsed port info
+ */
+function parseCliMessage(line) {
+    if (!line) return;
+    const parts = line.trim().split(/\s+/);
+    if (parts.length === 4) {
+        // Miss state
+        parts.splice(3, 0, '');
+    }
+    return {
+        protocal: parts[0] || '',
+        localAddress: parts[1] || '',
+        localPort: getPort(parts[1]),
+        remoteAddress: parts[2] || '',
+        remotePort: getPort(parts[2]),
+        state: parts[3] || '',
+        pid: parts[4] || '',
+    };
+}
+
+function getPortUsageInfo(port = '80', local = true, exact = false) {
+    return new Promise((resolve, reject) => {
+        exec(`netstat -ano | findstr "${port}"`, (err, stdout, stderr) => {
+            if (err) {
+                return stderr ? reject(err) : resolve([]);
+            }
+            resolve(
+                String(stdout)
+                    .split(/\r?\n/)
+                    .filter(v => !!v)
+                    .map(parseCliMessage)
+                    .filter(v => filterWithPort(v, port, local, exact))
+            );
+        });
+    });
+}
+
+function getProcessName(pid) {
+    return new Promise((resolve, reject) => {
+        exec(`tasklist -fi "PID eq ${pid}"`, (err, stdout, stderr) => {
+                if (err) return reject(err);
+                const result = String(stdout).trim()
+                    .split(/\r?\n/).slice(2)[0].split(/\s/);
+                resolve({
+                    pid,
+                    name: result[0] || '---',
+                    // memory: result.slice(-2).join('')
+                });
+            }
+        );
+    });
+}
+
+function killProcess(pid) {
+    return new Promise((resolve, reject) => {
+        exec(`taskkill -F -PID "${pid}"`, (err, stdout) => {
+            err ? reject(err) : resolve(stdout);
+        });
+    });
+}
+
+module.exports = {
+    getPortUsageInfo,
+    getProcessName,
+    killProcess
+};
